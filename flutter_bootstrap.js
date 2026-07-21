0fc9ab4115341f0f538e376704d7bc8de7440904
Victor_Tran <82859719+tranhieudevvietnam@users.noreply.github.com>
2026-07-21 16:42:49 +0700

    Add to Home Screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1721049321" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "448174392" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
