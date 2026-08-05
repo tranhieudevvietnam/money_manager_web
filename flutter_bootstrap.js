a470743bc39d00c4bd8ae8a35c005a181cd9017d
Victor_Tran <82859719+tranhieudevvietnam@users.noreply.github.com>
2026-08-05 18:01:59 +0700

    Deploy 2026-08-05T18:01:59.356271

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3783644234" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1296729577" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
